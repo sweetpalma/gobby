ee0305ce3a2523978aa0decf470a88772159087c
Sweet Palma <paul.marlow@ukr.net>
2026-06-05 15:03:01 +0200

    feat: added time function

---
+++ src/functions/getCurrentDateTime.ts
@@ -0,0 +1,17 @@
+import { defineChatSessionFunction } from 'node-llama-cpp';
+
+export const getCurrentDateTime = defineChatSessionFunction({
+  description: 'Get the current local date and time.',
+  params: {
+    type: 'object',
+    properties: {},
+  },
+  handler: async () => {
+    const now = new Date();
+    return {
+      iso: now.toISOString(),
+      local: now.toLocaleString(),
+      timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    };
+  },
+});
--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,1 +1,2 @@
 export * from './listDownloadedModels.js';
+export * from './getCurrentDateTime.js';
