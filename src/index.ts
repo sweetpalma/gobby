fb17b9eb907dc84650ff1cd648bd3ac83350a951
Sweet Palma <paul.marlow@ukr.net>
2026-07-09 20:20:42 +0200

    feat: added "memory" CLI command

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,13 @@
 import { homedir } from 'node:os';
 import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
-import { Command } from 'commander';
+import { Command } from '@commander-js/extra-typings';
 
 import * as functions from './functions';
 import { version } from '../package.json';
 import { Agent } from './agent';
 import { Config } from './utils/config';
 import { render } from './ui/terminal';
-
-// prettier-ignore
-const args = new Command()
-	.name('gobby')
-	.version(version)
-	.argument('[query...]')
-	.parse();
 
 const agent = new Agent({
 	functions,
@@ -24,20 +17,61 @@ const agent = new Agent({
 	}),
 });
 
-const main = async (initialPrompt?: string) => {
-	const tui = render({ agent, initialPrompt });
-	await agent.load();
-	await tui.waitUntilExit();
-};
+const examples = `
+Examples:
+  gobby 
+  gobby "What have I done in my last three commits?"
+  gobby memory show
+  gobby memory clear
+`;
 
-const query = args.args.join(' ').trim();
-const queryIsDefined = query.length > 0;
-if (process.stdin.isTTY) {
-	// prettier-ignore
-	const initialPrompt = queryIsDefined ? query : 'Hello! Greet me in a very short manner.';
-	main(initialPrompt);
-} else {
-	const piped = readFileSync(0).toString();
-	const initialPrompt = queryIsDefined ? `${query}\n${piped}` : piped;
-	main(initialPrompt);
-}
+const program = new Command()
+	.name('gobby')
+	.description('blazing fast gremlin agent that never leaves your machine')
+	.version(version);
+
+// prettier-ignore
+const commandMemory = program
+	.command('memory')
+	.description('manage agent memories');
+commandMemory
+	.command('show')
+	.description('show agent memories')
+	.action(async () => {
+		await agent.memory.load();
+		console.log(agent.memory.format());
+	});
+commandMemory
+	.command('clear')
+	.description('clear agent memories')
+	.action(async () => {
+		agent.memory.reset();
+		await agent.memory.save();
+	});
+
+// prettier-ignore
+program
+	.argument('[query...]')
+	.action(async (opts) => {
+		const main = async (initialPrompt?: string) => {
+			const tui = render({ agent, initialPrompt });
+			await agent.load();
+			await tui.waitUntilExit();
+		};
+		const query = opts.join(' ').trim();
+		const queryIsDefined = query.length > 0;
+		if (process.stdin.isTTY) {
+			// prettier-ignore
+			const initialPrompt = queryIsDefined ? query : 'Hello! Greet me in a very short manner.';
+			await main(initialPrompt);
+		} else {
+			const piped = readFileSync(0).toString();
+			const initialPrompt = queryIsDefined ? `${query}\n${piped}` : piped;
+			await main(initialPrompt);
+		}
+	});
+
+// prettier-ignore
+program
+	.addHelpText('after', examples)
+	.parse();
